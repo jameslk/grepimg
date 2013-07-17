e56003c89ee62a28ee24c6341fe1cef0dbd0f572
James Koshigoe <james@jameskoshigoe.com>
2013-07-16 19:12:51 -0600

    First!!

---
+++ README.md
@@ -0,0 +1,19 @@
+# Grepimg
+A hacked together Ruby script to search through image files for a piece of text, ala grep-style.
+
+## Installation
+Requires tesseract (for OCR) and imagemagick (for scaling to improve accuracy) to be installed on your system and depends on the `rtesseract` gem.
+
+## Usage
+Case-sensitive text search:
+	ruby grepimg.rb "Text to Find" *.jpg
+
+Case-insensitive text search:
+	ruby grepimg.rb -i "text to find" *.jpg
+
+Regular expression search (can be combined with -i):
+	ruby grepimg.rb -e "foo.*bar" *.jpg
+
+Piping in files to search from:
+	find . | ruby grepimg.rb foo
+
---
+++ grepimg.rb
@@ -0,0 +1,120 @@
+require 'rtesseract'
+
+class GrepimgText < Struct.new(:filename, :text)
+end
+
+class GrepimgCli
+	attr_accessor :ocr_engine, :image_resize_factor
+  attr_accessor :argv, :input, :pattern, :file_mask
+  attr_accessor :matcher, :is_case_insensitive
+
+	def initialize
+		self.matcher = :match_text
+	end
+
+	def ocr_engine
+		@ocr_engine ||= RTesseract
+  end
+
+  def new_text
+    GrepimgText.new
+  end
+
+  def image_resize_factor
+    @image_resize_factor ||= 3
+  end
+
+	def run(argv, input = [])
+		self.argv = argv
+    self.input = input
+		parse_args
+		find_matches
+	end
+
+protected
+	def filters
+		@filters ||= []
+	end
+
+	def parse_args
+    self.file_mask = input unless input.empty?
+
+		argv.each do |arg|
+			case arg
+        when '-i'
+          self.is_case_insensitive = true
+
+        when '-e'
+          self.matcher = :match_regex
+
+        else
+          push_param(arg)
+			end
+		end
+  end
+
+  def push_param(param)
+    if !@param_count
+      @param_count = 0
+      self.pattern = param
+    elsif @param_count == 1
+      self.file_mask = param
+    end
+
+    @param_count += 1
+  end
+
+  def image_text(filename)
+    ocr_image = ocr_engine.read(filename) do |image|
+      image.resize!(image_resize_factor) if image_resize_factor > 0
+    end
+    ocr_image.to_s
+  end
+
+	def each_file_text
+    Dir.glob(file_mask) do |filename|
+      begin
+        image_text(filename).split("\n").each do |line|
+          file_text = new_text
+
+          file_text.filename = filename
+          file_text.text = line
+
+          yield file_text
+        end
+      rescue
+        next
+      end
+    end
+  end
+
+	def find_matches
+    each_file_text do |file_text|
+      output_match file_text if send(matcher, file_text.text)
+    end
+	end
+
+	def output_match(file_text)
+    puts "File: #{file_text.filename}, Text: \n#{file_text.text}\n\n"
+	end
+
+	def match_text(text)
+    if is_case_insensitive
+      text = text.downcase
+      self.pattern = pattern.downcase
+    end
+
+    text.include? pattern
+	end
+
+	def match_regex(text)
+    if is_case_insensitive
+      text =~ /#{pattern}/i
+    else
+      text =~ /#{pattern}/
+    end
+	end
+end
+
+grepimg = GrepimgCli.new
+grepimg.run(ARGV, STDIN.tty? ? [] : STDIN.readlines.map(&:strip))
